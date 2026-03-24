1dc09796f643a88131f000d7ab4c686f8a6b46de
parsl3y <135058167+parsl3y@users.noreply.github.com>
2026-03-24 16:26:12 +0200

    fix: config file

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
 const nextConfig: NextConfig = {
-  output: 'export', 
+  ...(isGithubActions ? { output: 'export' } : {}),
   
   images: {
     unoptimized: true, 
