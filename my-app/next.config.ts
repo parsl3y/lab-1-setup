af53d09b986460110f37384b93f40b3dcd397d3e
parsl3y <135058167+parsl3y@users.noreply.github.com>
2026-03-24 16:11:09 +0200

    fix: config file

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export', 
+  
   images: {
     unoptimized: true, 
   },
-  basePath: '/lab-1-setup', 
+
+  basePath: isGithubActions ? '/lab-1-setup' : '',
+
   reactStrictMode: true,
 };
 
