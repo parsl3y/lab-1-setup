773649f5c051f5b26450777c870f3e24b896cd40
parsl3y <135058167+parsl3y@users.noreply.github.com>
2026-03-30 11:32:08 +0300

    jsdom pakage install

--- my-app/vitest.config.ts
+++ my-app/vitest.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': './src'
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
